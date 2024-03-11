ddd74da23341af1168c112f1b05fe3920cf7ed58
uesaka <j-uesaka@soft-hyperion.com>
2024-03-11 16:44:31 +0900

    waffawfa

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>{process.env.REACT_APP_HOGE}</p>
     </>
   )
 }
