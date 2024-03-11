31519942123a8d70dabfd02f32a2e22badf00e9b
uesaka <j-uesaka@soft-hyperion.com>
2024-03-11 17:52:01 +0900

    fawa

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>値は{process.env.REACT_APP_HOGE}</p>
+      <p>値は：{import.meta.env.VITE_HOGE}</p>
     </>
   )
 }
